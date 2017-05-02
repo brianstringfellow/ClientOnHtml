003787099baacd2759325b414b58deabc473eb38
Brian Stringfellow <brian.stringfellow@ge.com>
2017-05-02 16:37:41 -0700

    initial push

---
+++ CefSharp00/CefCustomObject.cs
@@ -0,0 +1,29 @@
+﻿namespace CefSharp00
+{
+   using CefSharp;
+   using CefSharp.WinForms;
+   using System.Diagnostics;
+
+   public class CefCustomObject
+   {
+      private static ChromiumWebBrowser _instanceBrowser;
+      private static Form1 _instanceForm;
+
+      public CefCustomObject(ChromiumWebBrowser originalBrowser, Form1 mainForm)
+      {
+         _instanceBrowser = originalBrowser;
+         _instanceForm = mainForm;
+      }
+
+      public void ShowDevTools()
+      {
+         _instanceBrowser.ShowDevTools();
+      }
+
+      public void OpenCmd()
+      {
+         ProcessStartInfo start = new ProcessStartInfo("cmd.exe", "/c pause");
+         Process.Start(start);
+      }
+   }
+}
---
+++ CefSharp00/Form1.cs
@@ -0,0 +1,47 @@
+﻿using CefSharp;
+using CefSharp.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CefSharp00
+{
+   public partial class Form1 : Form
+   {
+      public Form1()
+      {
+         InitializeComponent();
+      }
+
+      private void Form1_Load(object sender, EventArgs e)
+      {
+         CefSettings settings = new CefSettings();
+         Cef.Initialize(settings);
+
+         String pageAddress = $@"{Application.StartupPath}\HtmlResources\html\index.html";
+         ChromiumWebBrowser browser = new ChromiumWebBrowser(pageAddress);
+         Controls.Add(browser);
+         browser.Dock = DockStyle.Fill;
+
+         BrowserSettings browserSettings = new BrowserSettings
+         {
+            FileAccessFromFileUrls = CefState.Enabled,
+            UniversalAccessFromFileUrls = CefState.Enabled
+         };
+         browser.BrowserSettings = browserSettings;
+         browser.RegisterJsObject("cefCustomObject", new CefCustomObject(browser, this));
+      }
+
+      private void Form1_FormClosing(object sender, EventArgs e)
+      {
+         Cef.Shutdown();
+      }
+
+   }
+}
